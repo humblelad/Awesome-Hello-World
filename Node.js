cff92ac0fe6186f6ccf23287d2c8f2e7f6562732
The Lost Jedi <32588696+robocyber@users.noreply.github.com>
2018-06-25 20:02:30 +0530

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,8 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
